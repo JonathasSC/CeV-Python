43aa72347f720d9f3424ed6720cc3fb9d06b64b3
Jonathas Dos Santos <89261014+JonathasSC@users.noreply.github.com>
2022-04-19 10:37:39 -0300

    Update ex104.py

--- ex104.py
+++ ex104.py
@@ -1,1 +1,19 @@
-n = int(input('Digite um numero inteiro: '))
+def LeiaInt(texto):
+
+	n = input(texto)	
+
+	if n.isdecimal():
+
+		print(f'Boa! {n} é um numero valido')
+
+		
+
+	else:
+
+		print('Valor invalido')
+
+		return LeiaInt(texto)
+
+		
+
+n = LeiaInt('Ola digite um numero')
